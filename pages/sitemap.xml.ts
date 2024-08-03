fa596f7663706c901820549d6a033bcbefd1dc4d
DIEHARDERS <cybrobo@gmail.com>
2024-08-02 18:35:16 -0700

    Fix sitemap generator

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,6 +1,9 @@
 const EXTERNAL_DATA_URL = "https://jsonplaceholder.typicode.com/posts";
 
-function generateSiteMap(posts: any[]) {
+/**
+ * Fetch urls from an external source
+ */
+function generateDynamicSiteMap(posts: any[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
@@ -20,17 +23,33 @@ function generateSiteMap(posts: any[]) {
  `;
 }
 
+/**
+ * We manually set the two URLs we know already
+ */
+function generateStaticSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>https://www.openbrewai.com</loc>
+     </url>
+   </urlset>
+ `;
+}
+
 function SiteMap() {
   // getServerSideProps will do the heavy lifting
 }
 
+/**
+ * This page will hit our API to get data that will allow us to know the URLs of our dynamic pages. We will then write an XML file as the response for /sitemap.xml
+ */
 export async function getServerSideProps({ res }: any) {
   // We make an API call to gather the URLs for our site
-  const request = await fetch(EXTERNAL_DATA_URL);
-  const posts = await request.json();
+  // const request = await fetch(EXTERNAL_DATA_URL);
+  // const posts = await request.json();
 
   // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts);
+  const sitemap = generateStaticSiteMap();
 
   res.setHeader("Content-Type", "text/xml");
   // we send the XML to the browser
