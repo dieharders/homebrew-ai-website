1ebe2c350e6761f8964fdf0cf47516c26dd0021f
DIEHARDERS <cybrobo@gmail.com>
2024-08-02 18:28:55 -0700

    Add sitemap generator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import '@/global/global.css'
 
+// Overrides the default "App"
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
