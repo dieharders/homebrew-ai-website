a12be697815276840f10c53a8cde740fc72a2b8d
dieharders <cybrobo@gmail.com>
2026-04-05 13:14:11 -0700

    fix tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,13 @@
-import tailwind from "eslint-plugin-tailwindcss";
 import tsParser from "@typescript-eslint/parser";
 
 export default [
   {
     ignores: [".next/**", "node_modules/**"],
   },
-  ...tailwind.configs["flat/recommended"],
   {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       parser: tsParser,
-    },
-    settings: {
-      tailwindcss: {
-        config: "tailwind.config.js",
-      },
     },
   },
 ];
