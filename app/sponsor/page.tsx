6dd567829f927ed6d3c93326a84980c4a7087815
Sorob Raissi <cybrobo@gmail.com>
2026-08-05 16:11:57 -0700

    Enhancement/add motionbuff subdomain (#29)
    * create proper sitemap and add new motionbuff subdomain.
    allow links in the dropdown to be discoverable by google.
    
    * remove size css

--- app/sponsor/page.tsx
+++ app/sponsor/page.tsx
@@ -29,7 +29,6 @@ export default function SponsorPage() {
               alt=""
               width={128}
               height={128}
-              style={{ width: 128, height: "auto" }}
               className={styles.heroIcon}
               priority
             />
